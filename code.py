cc2dbacb7ea1ce45f66dade7aa6d1913261bea6b
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-05-14 09:23:34 -0700

    update of beautiful code

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # A function to calculate simple addtion maths
 
-def addition(num1, num2):
-       num3 =  num1 + num2
-       print(num3)
 
-addition(50,80)
+def addition(num1, num2):
+    num3 = num1+num2
+    print(num3)
+
+
+addition(50, 80)
